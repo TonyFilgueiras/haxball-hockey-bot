0e2ac39bc18e9a1c07f98b5bf1ee7764398fec2d
Tony Filgueiras <ant.mosquera@yahoo.com.br>
2023-08-28 20:10:58 -0300

    fixing touchedDisc bug

--- src/functions/touchedDisc.ts
+++ src/functions/touchedDisc.ts
@@ -3,7 +3,7 @@ import detectLastPlayerTouch from "./detectLastPlayerTouch";
 import { room } from "../bot";
 
 export default function touchedDisc( player: Player, penaltyMode: boolean = false): boolean {
-    if (room.discs[0].distanceTo(player) < 1) {
+    if (room.discs[0].distanceTo(player) < 0.1) {
         detectLastPlayerTouch(player, penaltyMode)
         return true
     } else {
