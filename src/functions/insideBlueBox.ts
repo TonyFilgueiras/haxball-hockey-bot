025edc36dd9a32326707ae8db9542821cf0d265f
Tony Filgueiras <ant.mosquera@yahoo.com.br>
2023-08-31 03:20:15 -0300

    adding inside goal check

--- src/functions/insideBlueBox.ts
+++ src/functions/insideBlueBox.ts
@@ -4,10 +4,24 @@ import distanceBetweenDots from "./math/distanceBetweenDots";
 export default function insideBlueBox(x: number, y: number): boolean {
     const radius = 124
     const xCenter = 757
-    if (x > xCenter + 3) {
+    
+    if (insideBlueGoal(x, y)) {
+        return true
+    } else if (x > xCenter + 6) {
         return false
     } else {
-        const distancia = distanceBetweenDots(x, y, xCenter, 0);
-        return distancia <= radius;
+        const distance = distanceBetweenDots(x, y, xCenter, 0);
+        return distance <= radius;
+    }
+}
+
+function insideBlueGoal(x: number, y: number): boolean{
+    const radius = 124
+    const xCenter = 757
+    const distance = distanceBetweenDots(x, y, xCenter, 0);
+    if (x < 800 && y < 81 && y > -81 && distance <= radius) {
+        return true
+    } else {
+        return false
     }
 }
--- src/functions/insideRedBox.ts
+++ src/functions/insideRedBox.ts
@@ -4,10 +4,23 @@ import distanceBetweenDots from "./math/distanceBetweenDots";
 export default function insideRedBox(x: number, y: number): boolean {
     const radius = 124
     const xCenter = -757
-    if (x < xCenter - 3) {
+    if (insideRedGoal(x, y)) {
+        return true
+    } else if (x < xCenter - 6) {
         return false
     } else {
         const distancia = distanceBetweenDots(x, y, xCenter, 0);
         return distancia <= radius;
+    }
+}
+
+function insideRedGoal(x: number, y: number): boolean{
+    const radius = 124
+    const xCenter = -757
+    const distance = distanceBetweenDots(x, y, xCenter, 0);
+    if (x > -800 && y < 81 && y > -81 && distance <= radius) {
+        return true
+    } else {
+        return false
     }
 }
