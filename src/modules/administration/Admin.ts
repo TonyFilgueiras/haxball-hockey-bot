0d18a804e6d12387466a2630be463518988a862f
TonyFilgueiras <ant.mosquera@yahoo.com.br>
2024-03-29 22:47:35 -0300

    fix: removing admin functions

--- src/modules/administration/Admin.ts
+++ src/modules/administration/Admin.ts
@@ -60,10 +60,6 @@ export class Admin extends Module {
     });
   }
 
-  private isAdmin(player: Player) {
-    return this.adminsAuth.includes(player.auth) || (player.roles.includes(Global.bypassRegisterRole) && player.isAdmin());
-  }
-
   private updateAdmins(room: Room) {
     if (!room.getPlayers().find((p) => p.isAdmin() && !p.settings.afk)) {
       const player = room
@@ -77,131 +73,10 @@ export class Admin extends Module {
   }
 
   @Command({
-    name: "trancar",
-  })
-  trancarCommand($: CommandInfo, room: Room) {
-    if (this.isAdmin($.caller)) {
-      room.setPassword("hfb");
-
-      return false;
-    }
-
-    $.caller.reply({ message: `⚠️ Somente administradores oficiais podem utilizar esse comando!`, color: Global.Color.Tomato, style: "bold" });
-
-    return false;
-  }
-
-  @Command({
-    name: "destrancar",
-  })
-  destrancarCommand($: CommandInfo, room: Room) {
-    if (this.isAdmin($.caller)) {
-      room.clearPassword();
-
-      return false;
-    }
-
-    $.caller.reply({ message: `⚠️ Somente administradores oficiais podem utilizar esse comando!`, color: Global.Color.Tomato, style: "bold" });
-
-    return false;
-  }
-
-  // @Command({
-  //   name: "admin",
-  // })
-  // adminCommand($: CommandInfo, room: Room) {
-  //   if (this.isAdmin($.caller)) {
-  //     $.caller.setAdmin(!$.caller.isAdmin());
-
-  //     return false;
-  //   }
-
-  //   $.caller.reply({ message: `⚠️ Somente administradores oficiais podem utilizar esse comando!`, color: Global.Color.Tomato, style: "bold" });
-
-  //   return false;
-  // }
-
-  @Command({
-    name: "limparbans",
-    aliases: ["clearbans"],
-  })
-  limparbansCommand($: CommandInfo, room: Room) {
-    if (!this.isAdmin($.caller)) {
-      $.caller.reply({ message: `⚠️ Somente administradores oficiais podem utilizar esse comando!`, color: Global.Color.Tomato, style: "bold" });
-
-      return false;
-    }
-
-    if (this.bans.length === 0 && $.args[0] !== "force") {
-      $.caller.reply({
-        message: `⚠️ Não há ninguém banido!\n❌ Se você acha que isso é um erro, digite "${room.prefix}limparbans force" para desbanir mesmo assim.`,
-        color: Global.Color.Tomato,
-        style: "bold",
-      });
-
-      return false;
-    }
-
-    room.clearBans();
-    room.send({
-      message: `🕊️ Todos os bans (${this.bans.length}) foram removidos por ${$.caller.name}!`,
-      color: Global.Color.HotPink,
-      style: "bold",
-    });
-
-    this.bans = [];
-  }
-
-  @Command({
-    name: "desbanir",
-  })
-  desbanirCommand($: CommandInfo, room: Room) {
-    if (!this.isAdmin($.caller)) {
-      $.caller.reply({ message: `⚠️ Somente administradores oficiais podem utilizar esse comando!`, color: Global.Color.Tomato, style: "bold" });
-
-      return false;
-    }
-
-    const idOrName = $.args[0];
-    const bannedPlayers = this.bans.filter((p) => p.id === parseInt(idOrName) || p.name === idOrName);
-
-    if (bannedPlayers.length) {
-      let names: string[] = [];
-      let message = "";
-
-      bannedPlayers.forEach((bP) => {
-        names.push(bP.name);
-        room.clearBan(bP.id);
-        this.bans = this.bans.filter((p) => p.id !== bP.id);
-      });
-
-      if (names.length > 1) {
-        if (names.every((value, index, array) => value === array[0])) {
-          message = `${names.length} jogadores com o nome ${names[0]} foram desbanidos`;
-        } else {
-          message = `${Utils.getPlayersNames(bannedPlayers)} foram desbanidos`;
-        }
-      } else {
-        message = `${names[0]} foi desbanido`;
-      }
-
-      room.send({ message: `🕊️ ${message} por ${$.caller.name}`, color: Global.Color.HotPink, style: "bold" });
-    } else {
-      $.caller.reply({ message: `⚠️ Não foi encontrado nenhum jogador banido com esse nome ou ID!`, color: Global.Color.Tomato, style: "bold" });
-    }
-  }
-
-  @Command({
     name: "banidos",
     aliases: ["bans"]
   })
   banidosCommand($: CommandInfo, room: Room) {
-    if (!this.isAdmin($.caller)) {
-      $.caller.reply({ message: `⚠️ Somente administradores oficiais podem utilizar esse comando!`, color: Global.Color.Tomato, style: "bold" });
-
-      return false;
-    }
-
     if (this.bans.length) {
       const banned = this.bans.slice(0, 20);
 
